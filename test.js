ca2bbb85a5e08cd2591e7c123dd82c79c0b94d3a
Nenad005 <markonenadovic2005@gmail.com>
2023-12-17 00:12:00 +0100

    male

--- test.js
+++ test.js
@@ -1,30 +1,87 @@
-function downloadInnerHtmlByClassName(className, filename) {
-    const elements = document.getElementsByClassName(className);
-    if (elements.length === 0) {
-      console.error('No elements found with class name "' + className + '".');
-      return;
+
+const actions = {
+    follow : 0,
+    unfollow : 1,
+    like : 2
+}
+
+const restTime = {
+    pocetak: 2,
+    kraj : 14,
+}
+const maxActionsPerDay = 150
+
+function follow(){console.log('Followed')}
+function unfollow(){console.log('Unfollowed')}
+function like(){console.log('Liked')}
+
+function perform_action(action_type){
+    switch (action_type) {
+        case 0:
+            follow()
+            break
+      
+        case 1:
+            unfollow()
+            break
+
+        case 2:
+            like()
+            break
+
+        default:
+            console.log("Invalid action")
+            break
     }
-  
-    let combinedHtml = '';
-  
-    for (const element of elements) {
-      combinedHtml += element.innerHTML + '\n';
+}
+
+function randInt(max, min = 0){
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function timeUntilRest(){
+    if (isRestTime()) return -1
+
+    const now = new Date();
+    const target = new Date(now);
+    target.setHours(restTime.pocetak, 0, 0, 0);
+
+    if (now > target) {
+        target.setDate(target.getDate() + 1);
     }
-  
-    const blob = new Blob([combinedHtml], { type: 'text/html' });
-    const url = URL.createObjectURL(blob);
-  
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename;
-  
-    document.body.appendChild(a);
-    a.click();
-  
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  }
-  
-  // Replace 'elementsToDownload' with the actual class name of the elements and 'downloaded_content.html' with the desired filename
-  downloadInnerHtmlByClassName('_ap3a _aaco _aacw _aacx _aad7 _aade', 'downloaded_content.html');
-  
+
+    const timeDifference = Math.floor((target - now) / (1000 * 60));
+
+    return timeDifference;
+}
+
+function isRestTime() {
+    const currentHour = new Date().getHours();
+    return currentHour >= restTime.pocetak && currentHour < restTime.kraj;
+}
+
+function timeUntilRestEnd(){
+    if (!isRestTime()) return -1
+    const now = new Date();
+    const target = new Date(now);
+    target.setHours(restTime.kraj, 0, 0, 0);
+
+    if (now > target) {
+        target.setDate(target.getDate() + 1);
+    }
+
+    const timeDifference = Math.floor((target - now) / (1000 * 60));
+
+    return timeDifference;
+}
+
+function initialize(){
+    // a = randInt(2, 0)
+    // console.log(a)
+    // perform_action(a)
+    console.log(timeUntilRest())
+    console.log(timeUntilRestEnd())
+}
+
+
+initialize()
